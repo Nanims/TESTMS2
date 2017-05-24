7e60f996950995fb2626f8bf3688e2c7e3553ca4
lnc <lnc@DIN76000161.corp.capgemini.com>
2017-05-24 17:55:49 +0530

    rrtert

---
+++ TESTMS2/src/Hello.java
@@ -0,0 +1,14 @@
+import main.java.SalesClass;
+
+public class Hello {
+	public static void display(){
+		System.out.println("Hey!");
+		
+		System.out.println("Greetings!  Good After Noon!");
+	}
+	
+	public static void main(String[] args) {
+		display();
+	
+	}
+}
